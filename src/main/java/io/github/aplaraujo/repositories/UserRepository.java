5fa0b9b561143c498cf07d06aafdcf5d940428ef
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-19 20:57:52 -0300

    Authentication service

--- src/main/java/io/github/aplaraujo/repositories/UserRepository.java
+++ src/main/java/io/github/aplaraujo/repositories/UserRepository.java
@@ -2,8 +2,11 @@ package io.github.aplaraujo.repositories;
 
 import io.github.aplaraujo.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByNameAndLogin(String name, String login);
+
+    UserDetails findByLogin(String login);
 }
---
+++ src/main/java/io/github/aplaraujo/services/impl/AuthServiceImpl.java
@@ -0,0 +1,20 @@
+package io.github.aplaraujo.services.impl;
+
+import io.github.aplaraujo.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return userRepository.findByLogin(login);
+    }
+}
