0b8960f0b80678f9d59969aef210d59b3d0ef85e
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-12-19 21:39:28 -0300

    Security configuration

---
+++ src/main/java/io/github/aplaraujo/config/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package io.github.aplaraujo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers(HttpMethod.GET, "/users").hasRole("ADMIN") // Somente quem tem o perfil de administrador e está autenticado pode acessar essa rota
+                                .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(16);
+    }
+}
--- src/main/java/io/github/aplaraujo/services/impl/UserServiceImpl.java
+++ src/main/java/io/github/aplaraujo/services/impl/UserServiceImpl.java
@@ -6,17 +6,16 @@ import io.github.aplaraujo.entities.User;
 import io.github.aplaraujo.mappers.UserMapper;
 import io.github.aplaraujo.repositories.UserRepository;
 import io.github.aplaraujo.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
-
-    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
-        this.userRepository = userRepository;
-        this.userMapper = userMapper;
-    }
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserResponseDTO save(UserRequestDTO dto) {
@@ -24,7 +23,9 @@ public class UserServiceImpl implements UserService {
         if (doesTheUserExist != null) {
             throw new RuntimeException("This user already exists!");
         }
+        var passwordHash = passwordEncoder.encode(dto.password());
         User user = userMapper.toEntity(dto);
+        user.setPassword(passwordHash);
         user = userRepository.save(user);
         return userMapper.toDTO(user);
     }
